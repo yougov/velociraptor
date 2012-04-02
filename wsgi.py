d6ac51a3d08cab2b08f041e47683468966002a05
Brent Tubbs <brent.tubbs@gmail.com>
2012-04-02 16:54:05 -0700

    add wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+import django.core.handlers.wsgi
+import os
+import sys
+
+# This file is a shim to expose a Django project as a WSGI application that can
+# be run by gunicorn or other things that speak WSGI.  
+
+here = os.path.dirname(os.path.realpath(__file__))
+# add parent dir to path so things like velociraptor.urls will resolve.
+sys.path.insert(0, os.path.dirname(here))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+app = django.core.handlers.wsgi.WSGIHandler()
