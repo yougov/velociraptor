4750ff27cb0c64af5488a71fe3705364c9f6e17b
Brent Tubbs <brent.tubbs@gmail.com>
2012-07-13 14:06:27 -0700

    remove LocalNginxBalancer

--- project/deployment/balancer/nginx.py
+++ project/deployment/balancer/nginx.py
@@ -1,16 +1,14 @@
 import errno
 import logging
-import os
 import posixpath
 import random
 import re
 import string
-import subprocess
-import tempfile
 import warnings
 
 import ssh
 from django.conf import settings
+
 
 # Template for 'upstream' directive in nginx config.
 UPSTREAM_TPL = """
@@ -41,74 +39,10 @@ def str_to_pool(upstream):
     return name, nodes
 
 
-class tmpfile(object):
-    def __enter__(self):
-        self.file = tempfile.mkstemp()
-        return os.fdopen(self.file[0], 'w'), self.file[1]
-    def __exit__(self, type, value, traceback):
-        os.remove(self.file[1])
-
-
-class LocalNginxBalancer(object):
-    """
-    A Velociraptor balancer backend for writing local nginx config.  Should be
-    run as a user who doesn't need to enter a password for 'sudo'.  Not
-    suitable for high availability environments, since it only supports a
-    single nginx instance.
-    """
-
-    # User running Velociraptor must have write permissions on the include_dir.
-    def __init__(self, config=None):
-        config = {} if config is None else config
-        self.include_dir = config.get('include_dir',
-                                      '/etc/nginx/sites-enabled/')
-        self.reload_cmd = config.get('reload_cmd', 'sudo /etc/init.d/nginx '
-                                     'reload')
-
-    def _get_filename(self, pool):
-        return os.path.join(self.include_dir, pool) + '.conf'
-
-    def _write_pool(self, pool, nodes):
-        filename = self._get_filename(pool)
-        with tmpfile() as tmp:
-            f, tmpname = tmp
-            contents = pool_to_str(pool, list(nodes))
-            print contents
-            f.write(contents)
-            f.close()
-            cmd = 'sudo cp %s %s' % (tmpname, filename)
-            subprocess.call(cmd.split())
-
-    def add_nodes(self, pool, nodes):
-        existing_nodes = set(self.get_nodes(pool))
-        self._write_pool(pool, existing_nodes.union(nodes))
-        # reload nginx config.  Requires sudo
-        subprocess.call(self.reload_cmd.split())
-
-    def get_nodes(self, pool):
-        filename = self._get_filename(pool)
-        cmd = 'sudo cat ' + filename
-        try:
-            upstring = subprocess.check_output(cmd.split())
-            written_pool, nodes = str_to_pool(upstring)
-            if pool != written_pool:
-                logging.error('Pool file %s contains mismatched name %s' %
-                              (filename, written_pool))
-            return nodes
-        except subprocess.CalledProcessError:
-            # missing file
-            return []
-
-    def delete_nodes(self, pool, nodes):
-        existing_nodes = set(self.get_nodes(pool))
-        self._write_pool(pool, existing_nodes.difference(nodes))
-        subprocess.call(self.reload_cmd.split())
-
-
 class NginxBalancer(object):
     """
     A Velociraptor balancer backend for writing nginx config on remote servers.
-    Uses Fabric for writing files and running reload cmds.
+    Uses sftp and ssh for writing files and running reload cmds.
     """
 
     def __init__(self, config=None):
