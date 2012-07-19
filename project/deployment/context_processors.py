a6c7459ff296082e7b11417e263d13ea560cd782
Brent Tubbs <brent.tubbs@gmail.com>
2012-07-19 11:00:04 -0700

    pass squads into context processor

--- project/deployment/context_processors.py
+++ project/deployment/context_processors.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 
-from deployment.models import Swarm
+from deployment.models import Swarm, Squad
 
 
 # For showing a list of all swarms in the nav
 def raptor(request):
     return {
         'swarms': Swarm.objects.filter(active=True),
+        'squads': Squad.objects.all(),
         # Don't show web log links if syslogging is enabled
         'log_links': not settings.PROC_SYSLOG,
     }
