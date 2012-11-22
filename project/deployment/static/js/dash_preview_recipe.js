3fe38e6bcfd296ee6da27f8ef081b3459865587d
fernando.gutierrez <none@none>
2012-11-22 11:45:33 -0500

    Added a config-preview div instead of an alert for the preview config recipe action in the swarm form.

--- project/deployment/static/js/dash_preview_recipe.js
+++ project/deployment/static/js/dash_preview_recipe.js
@@ -1,14 +1,15 @@
-
 //// Fire in the hole! ////
 $(document).ready(function(){
     $('#id_recipe_id').after(" <a id=\"preview_recipe\" href=\"#\">Preview</a>");
     $('#preview_recipe').click(function() {
         var selected = $("#id_recipe_id option:selected").val();
         $.get('/preview_recipe/' + selected + '/', function(data) {
-            alert(data);
+            if ($('#config-preview').length) {
+                $('#config-preview').html('<h3>Config Preview</h3><pre>' + data + '</pre></div>');
+            } else {
+            $('.row-fluid').after('<div id="config-preview"><h3>Config Preview</h3><pre>' + data + '</pre></div>');
+            }
         });
         return false;
     });
-
-
 });
