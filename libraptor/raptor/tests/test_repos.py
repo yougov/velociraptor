47c57ca9a81d20126252b5b589939f3f0e51624d
Brent Tubbs <brent.tubbs@gmail.com>
2012-12-11 11:57:42 -0800

    include tarballed sample repos for tests

--- libraptor/raptor/tests/test_repos.py
+++ libraptor/raptor/tests/test_repos.py
@@ -1,9 +1,11 @@
 import os
+import tempfile
+import shutil
 
 import envoy
 
 from raptor import repo
-from raptor.util import tmpdir
+from raptor.util import tmpdir, CommandException
 
 
 def test_hg_folder_detection():
@@ -62,84 +64,84 @@ def test_git_clone():
 
 
 def test_hg_update():
-    url = 'https://bitbucket.org/btubbs/vr_python_example'
     newrev = '13b6ce1e234a'
     oldrev = '496e15fd973f'
-    with tmpdir():
-        hgrepo = repo.Repo('hgrepo', url, 'hg')
-        hgrepo.clone()
-        hgrepo.update(newrev)
-        f = os.path.join('hgrepo', 'newfile')
+    with tmprepo('hg_python_app.tar.gz', 'hg') as r:
+        r.update(newrev)
+        f = 'newfile'
         assert os.path.isfile(f)
 
-        hgrepo.update(oldrev)
+        r.update(oldrev)
         assert not os.path.isfile(f)
 
 
 def test_git_update():
-    url = 'https://github.com/btubbs/vr_python_example.git'
     newrev = '6c79fb7d071a9054542114eea70f69d5361a61ff'
     oldrev = '16c1dba07ee78d5dbee1f965d91d3d61942ccb67'
-    with tmpdir():
-        gitrepo = repo.Repo('gitrepo', url, 'git')
-        gitrepo.clone()
-        gitrepo.update(newrev)
-        f = os.path.join('gitrepo', 'newfile')
+    with tmprepo('git_python_app.tar.gz', 'git') as r:
+        r.update(newrev)
+        f = 'newfile'
         assert os.path.isfile(f)
 
-        gitrepo.update(oldrev)
+        r.update(oldrev)
         assert not os.path.isfile(f)
 
 
 def test_hg_update_norev():
-    url = 'https://bitbucket.org/btubbs/vr_python_example'
     newrev = '13b6ce1e234a'
     oldrev = '496e15fd973f'
-    with tmpdir():
-        hgrepo = repo.Repo('hgrepo', url, 'hg')
-        hgrepo.clone()
-        hgrepo.update(newrev)
-        f = os.path.join('hgrepo', 'newfile')
-        assert os.path.isfile(f)
-
-        hgrepo.update(oldrev)
-        assert not os.path.isfile(f)
-
-        hgrepo.update()
-        assert os.path.isfile(f)
+    with tmprepo('hg_python_app.tar.gz', 'hg') as r:
+        r.update(newrev)
+        r.update(oldrev)
+        r.update()
+        assert os.path.isfile('newfile')
 
 
 def test_git_update_norev():
-    url = 'https://github.com/btubbs/vr_python_example.git'
     newrev = '6c79fb7d071a9054542114eea70f69d5361a61ff'
     oldrev = '16c1dba07ee78d5dbee1f965d91d3d61942ccb67'
-    with tmpdir():
-        gitrepo = repo.Repo('gitrepo', url, 'git')
-        gitrepo.clone()
-        gitrepo.update(newrev)
-        f = os.path.join('gitrepo', 'newfile')
-        assert os.path.isfile(f)
-
-        gitrepo.update(oldrev)
-        assert not os.path.isfile(f)
-
-        gitrepo.update()
-        assert os.path.isfile(f)
+    with tmprepo('git_python_app.tar.gz', 'git') as r:
+        r.update(newrev)
+        r.update(oldrev)
+        r.update()
+        assert os.path.isfile('newfile')
 
 
 def test_hg_get_version():
-    url = 'https://bitbucket.org/btubbs/vr_python_example'
     rev = '496e15fd973f'
-    with tmpdir():
-        hgrepo = repo.Repo('hgrepo', url, 'hg')
-        hgrepo.update(rev)
-        assert hgrepo.version == rev
+    with tmprepo('hg_python_app.tar.gz', 'hg') as r:
+        r.update(rev)
+        assert r.version == rev
 
 
 def test_git_get_version():
-    url = 'https://github.com/btubbs/vr_python_example.git'
     rev = '16c1dba07ee78d5dbee1f965d91d3d61942ccb67'
-    with tmpdir():
-        gitrepo = repo.Repo('gitrepo', url, 'git')
-        gitrepo.update(rev)
-        assert gitrepo.version == rev
+    with tmprepo('git_python_app.tar.gz', 'git') as r:
+        r.update(rev)
+        assert r.version == rev
+
+
+class tmprepo(object):
+    """Context manager for creating a tmp dir, unpacking a specified repo
+    tarball inside it, cd-ing in there, letting you run stuff, and then
+    cleaning up and cd-ing back where you were when it's done.
+    """
+    def __init__(self, tarball, vcs_type):
+        # Repo tarballs must be in the same directory as this file.
+        here = os.path.dirname(os.path.abspath(__file__))
+        self.tarball = os.path.join(here, tarball)
+        self.vcs_type = vcs_type
+        self.orig_path = os.getcwd()
+
+    def __enter__(self):
+        self.temp_path = tempfile.mkdtemp()
+        os.chdir(self.temp_path)
+        cmd = 'tar -zxf %s --strip-components 1' % self.tarball
+        result = envoy.run(cmd)
+        if result.status_code != 0:
+            raise CommandException(result)
+        return repo.Repo('./', vcs_type=self.vcs_type)
+
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.orig_path)
+        shutil.rmtree(self.temp_path, ignore_errors=True)
