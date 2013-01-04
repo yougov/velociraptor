2982071efba902b679da8505ad73431efd373ff8
Brent Tubbs <brent.tubbs@gmail.com>
2013-01-04 11:29:41 -0800

    bump

--- libraptor/setup.py
+++ libraptor/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='raptor',
-    version='2.5.4',
+    version='2.5.5',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
