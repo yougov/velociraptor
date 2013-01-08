0be646894af65d8692f805371038fcef9b312898
Brent Tubbs <brent.tubbs@gmail.com>
2013-01-07 16:45:49 -0800

    don't try to test check_current method that doesn't exist anymore

--- project/tests/test_build.py
+++ project/tests/test_build.py
@@ -18,54 +18,23 @@ def test_builds_get_current():
     a.save()
     now = timezone.now()
     tag = randchars()
-    check_mock = lambda x, y: True
-    with mock.patch('deployment.models.BuildPack.check_current', check_mock):
-        bp_url = randurl()
-        bp_version = randchars()
-        bp = models.BuildPack(repo_url=bp_url, repo_type='hg', order=0)
-        bp.save()
+    bp_url = randurl()
+    bp_version = randchars()
+    bp = models.BuildPack(repo_url=bp_url, repo_type='hg', order=0)
+    bp.save()
 
-        b = models.Build(app=a,
-                  tag=tag,
-                  file='abc',
-                  start_time=now,
-                  end_time=now,
-                  status='success',
-                  buildpack_url=bp_url,
-                  buildpack_version=bp_version,
-                 )
-        b.save()
+    b = models.Build(app=a,
+              tag=tag,
+              file='abc',
+              start_time=now,
+              end_time=now,
+              status='success',
+              buildpack_url=bp_url,
+              buildpack_version=bp_version,
+             )
+    b.save()
 
-        assert models.Build.get_current(a, tag).id == b.id
-
-
-def test_buildpack_not_current():
-    # Builds done with a non-current version of the buildpack should not be
-    # reused.
-    app_url = randurl()
-    a = models.App(name=randchars(), repo_url=app_url, repo_type='hg')
-    a.save()
-    now = timezone.now()
-    tag = randchars()
-    check_mock = lambda x, y: False
-    with mock.patch('deployment.models.BuildPack.check_current', check_mock):
-        bp_url = randurl()
-        bp_version = randchars()
-        bp = models.BuildPack(repo_url=bp_url, repo_type='hg', order=0)
-        bp.save()
-
-        b = models.Build(app=a,
-                  tag=tag,
-                  file='abc',
-                  start_time=now,
-                  end_time=now,
-                  status='success',
-                  buildpack_url=bp_url,
-                  buildpack_version=bp_version,
-                 )
-        b.save()
-
-        assert models.Build.get_current(a, tag) is None
+    assert models.Build.get_current(a, tag).id == b.id
 
 
 def test_build_usable():
