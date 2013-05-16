66ae48e80dcd5ba3b1863e33228b53bb60e78e78
Brent Tubbs <brent.tubbs@gmail.com>
2013-05-15 21:40:29 -0700

    lib2.15.1 add explicit mercurial dep to libraptor

--- libraptor/setup.py
+++ libraptor/setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='raptor',
-    version='2.15',
+    version='2.15.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Fabric==1.4.0',
-        'paramiko>=1.8.0,<2.0',
-        'suds==0.4',
-        'redis>=2.6.2',
         'isodate>=0.4.4',
+        'mercurial==2.6.1',
+        'paramiko>=1.8.0,<2.0',
+        'redis>=2.6.2',
+        'suds==0.4',
     ],
     entry_points={
         'console_scripts': [
