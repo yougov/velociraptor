ac55989e2a1ce5a3a7d285ce0de2310847d14aa2
Brent Tubbs <brent.tubbs@gmail.com>
2013-08-07 15:03:58 -0700

    bump vr

--- vr_django/setup.py
+++ vr_django/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='vr',
-    version='2.1.1',
+    version='2.1.2',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=['vr',
