009f19c279d3ea8ded40c912de712898ecdaf995
Jason R. Coombs <jaraco@jaraco.com>
2013-09-04 09:47:17 -0400

    Use --clean when updating the repo. Fixes #62.

--- common/vr/common/repo.py
+++ common/vr/common/repo.py
@@ -103,7 +103,7 @@ class Repo(object):
         with chdir(self.folder):
             if self.vcs_type == 'hg':
                 self.run('hg pull %s' % self.url)
-                self.run('hg up %s' % rev)
+                self.run('hg up --clean %s' % rev)
             elif self.vcs_type == 'git':
                 # NOTE: We don't need the url for git b/c the pull
                 #       didn't prompt for a password.
