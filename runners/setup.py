96364b75267597fd2064352b76b607d711318649
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-24 12:10:46 -0700

    bump runners

--- runners/setup.py
+++ runners/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.runners',
     namespace_packages=['vr'],
-    version='0.0.3',
+    version='0.0.4',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
