dcc2c73649e5f8eafde817c03f5f853ece8dde47
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-25 15:36:29 -0700

    server bump

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='3.4.1',
+    version='3.4.2',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
