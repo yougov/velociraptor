097befd40f0e8d25831832d97c8c6a48ecb1c4b7
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-25 15:48:04 -0700

    bump server

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='3.4.3',
+    version='3.4.4',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
@@ -33,7 +33,7 @@ setup(
         'South==0.7.6',
         'sseclient==0.0.6',
         'vr.common',
-        'vr.runners',
+        'vr.runners>=0.0.7',
     ],
     dependency_links = [
         'https://bitbucket.org/yougov/velociraptor/downloads/django-tastypie-0.9.12-alpha.tar.gz#egg=django-tastypie-0.9.12-alpha',
