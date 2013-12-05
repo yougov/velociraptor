cd67e51c7888285e2f2e6aaca5a3a72cc8857f72
Brent Tubbs <brent.tubbs@gmail.com>
2013-12-05 11:45:13 -0800

    bump runner

--- runners/setup.py
+++ runners/setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name='vr.runners',
     namespace_packages=['vr'],
-    version='0.0.12',
+    version='0.0.13',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     url='https://bitbucket.org/yougov/velociraptor',
     install_requires=[
-        'vr.common>=3.5.6,<4',
+        'vr.common>=3.6.1,<4',
         'requests>=1.2.0',
         'PyYAML>=3.10',
     ],
