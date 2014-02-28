08ddecee1a7f257bfb388d4d3e0f438fed12ef6e
Jason R. Coombs <jaraco@jaraco.com>
2014-02-28 17:40:56 -0500

    Bump

--- runners/setup.py
+++ runners/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.runners',
     namespace_packages=['vr'],
-    version='2.0.1',
+    version='2.0.2',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
