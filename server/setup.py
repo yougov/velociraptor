f878e736b68af7708b98d365650566eabd51058e
Josh Harbaugh <josh.harbaugh@yougov.com>
2014-07-28 16:35:55 -0400

    UI: Filter ingredients list on /ingredient endpoint.

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='3.11',
+    version='3.11.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
