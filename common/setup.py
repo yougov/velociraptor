eb739123a7681a7ab0451b870e2683d52edd17d7
Bryan Deeney <bdeeney@pobox.com>
2014-08-07 13:34:32 -0400

    Bump common

--- common/setup.py
+++ common/setup.py
@@ -11,7 +11,7 @@ py2_reqs = ['suds==0.4'] if not PY3 else []
 params = dict(
     name='vr.common',
     namespace_packages=['vr'],
-    version='3.9.1',
+    version='3.10',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=setuptools.find_packages(),
