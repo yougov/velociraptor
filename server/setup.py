7c82565eb3331c1f0c646f77accf44a3037b04b4
Bryan Deeney <bdeeney@pobox.com>
2014-09-15 11:41:22 -0400

    Bump server

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='3.13.3',
+    version='3.13.4',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
