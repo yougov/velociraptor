30a5f8e7e5857f33bf70a9eeb61b7589fee088d4
Josh Harbaugh <josh.harbaugh@yougov.com>
2014-10-09 23:01:28 -0500

    Bump server

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='3.14.2',
+    version='3.14.3',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
