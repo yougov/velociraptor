ab0772d9398fbce7a74e68c77e3ac3f769c9fdbf
Brent Tubbs <brent.tubbs@gmail.com>
2014-11-07 12:21:05 -0800

    bump builder

--- builder/setup.py
+++ builder/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='vr.builder',
     namespace_packages=['vr'],
-    version='1.0.5',
+    version='1.0.6',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
