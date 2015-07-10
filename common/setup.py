f64d1c1d1400655c4a132f95c0a3a46f528d2677
Jason R. Coombs <jaraco@jaraco.com>
2015-07-10 10:12:38 -0400

    Use setuptools extras and environment markers for declarative dependency on suds 0.4 (which fails under Python 3).

--- common/setup.py
+++ common/setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 
-import sys
-
 import setuptools
-
-PY3 = sys.version_info >= (3,)
-
-py2_reqs = ['suds==0.4'] if not PY3 else []
 
 params = dict(
     name='vr.common',
@@ -27,7 +21,13 @@ params = dict(
         'requests',
         'PyYAML>=3.10',
         'sseclient==0.0.8',
-    ] + py2_reqs,
+    ],
+
+    extras_require={
+        ':python_version=="2.7"': [
+            'suds==0.4',
+        ],
+    },
 )
 
 if __name__ == '__main__':
