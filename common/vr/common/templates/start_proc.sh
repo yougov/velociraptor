40e491e439434734a618bfb988464c034e791c34
Jason R. Coombs <jaraco@jaraco.com>
2015-07-11 05:34:13 -0400

    Split component packages into independent repositories. Use subrepos to keep expectation of presence of those packages in parent repository.

--- common/vr/common/templates/start_proc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# For apps running in containers, this script will set up the container and
-# then call the script to start the actual app.
-
-# For uncontained apps, this script will do almost nothing, just calling the
-# script with the actual command.
-%(cmd)s 
--- server/vr/server/balancer/varnish.py
+++
@@ -1,3 +0,0 @@
-# This code has been moved to libraptor.  This file now exists only for
-# backwards compatibility.
-from vr.common.balancer.varnish import VarnishBalancer
