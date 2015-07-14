1c5726da48401e1f1c5fe82192881e5125f36670
Jason R. Coombs <jaraco@jaraco.com>
2015-07-14 11:51:03 -0400

    Adding sphinx conf

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# General information about the project.
+project = 'Velociraptor'
+copyright = '2015 YouGov, Plc.'
+
+# The short X.Y version.
+version = '4.1'
+# The full version, including alpha/beta/rc tags.
+release = version
+
+master_doc = 'index'
